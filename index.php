e2b3997dc17296f3db952a1027dd582b86657b63
rahmadxhidayat1 <92037828+rahmadxhidayat1@users.noreply.github.com>
2022-07-14 00:38:02 +0700

    new
    haha

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/opening.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+		integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+		integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
+</head>
+<body>
+    <header>
+        <video id="bg-video" autoplay muted loop>
+            <source src="assets/video/opening.mp4" type="video/mp4">
+        </video>
+        <div class="pt-2 vid_info">
+            <h1 class="heading">hi everyone</h1>
+            <h2>WELCOME TO MY COMPANY</h2>
+            <br>
+            <div class="containers content">
+                <div class="kolom">
+                    <div class="atas">
+                        <a href="index.php"><img src="assets/img/logo.png" alt=""></a>
+                    </div>
+                    <div class="tengah">
+                        <h2 class="resmi">CUSTOMER</h2>
+                    </div>
+                    <div class="bawah">
+                        <p class="normal m-2 fs-4">For buy and rent container in TRI GUNA ABADI 
+                        </p>
+                    </div>
+                </div>
+                <div class="kolom">
+                    <div class="atas">
+                        <img src="assets/img/logo.png" alt="">
+                    </div>
+                    <div class="tengah">
+                        <h2 class="resmi">ADMIN</h2>
+                    </div>
+                    <div class="bawah">
+                        <p class="normal m-2 fs-4">For setting and Controlling database in website
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+</body>
+</html>
